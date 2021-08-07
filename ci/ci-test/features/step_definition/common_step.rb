5c475aaa392a72b0e87e363d9af4b8840f819def
Hariprakash94 <30706291+Hariprakash94@users.noreply.github.com>
2021-08-07 07:18:51 -0400

    grid env param and remove pending

--- ci/ci-test/features/step_definition/common_step.rb
+++ ci/ci-test/features/step_definition/common_step.rb
@@ -7,21 +7,21 @@ When(/^I click on "([^"]*)" button$/) do |arg|
 end
 
 And(/^I fill the user details$/) do
-  pending
+
 end
 
 And(/^I set the (\d+) length long password for the account$/) do |arg|
-  pending
+
 end
 
 Then(/^I land on the "([^"]*)" page$/) do |arg|
-  pending
+
 end
 
 And(/^I fill the user details for "([^"]*)" customer$/) do |arg|
-  pending
+
 end
 
 Then(/^I should see the error message "([^"]*)" for "([^"]*)"$/) do |arg1, arg2|
-  pending
+
 end
