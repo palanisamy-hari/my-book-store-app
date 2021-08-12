da69fddb57d0e4ad2ee7b81158897b81ec27dc62
Hariprakash94 <30706291+Hariprakash94@users.noreply.github.com>
2021-08-11 20:32:40 -0400

    selenium url

--- ci/ci-test/features/support/env.rb
+++ ci/ci-test/features/support/env.rb
@@ -5,4 +5,4 @@ require 'active_support'
 require 'rspec'
 
 ENV['FILE_NAME'] = 'test.mp4'
-ENV['SELENIUM-GRID-HOST']="http://18.222.40.22:4444/"
+ENV['SELENIUM-GRID-HOST']="http://3.144.100.236:4444"
