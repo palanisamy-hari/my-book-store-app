5fb05de9bb30b0cc5cbcacdcdd7cab7d20a2b794
Hariprakash94 <30706291+Hariprakash94@users.noreply.github.com>
2021-08-12 23:21:20 -0400

    final product

---
+++ README.md
@@ -0,0 +1,26 @@
+## Book Store Application
+
+This application was developed using NodeJS and MongoDB Backend Service.
+
+## Local execution
+
+1. ```npm install```
+
+2. ```npm start```
+
+this should start the application on http://localhost:5000  
+
+## Remote execution
+This application is deployed to heroku as well.
+
+https://simple-book-store-app.herokuapp.com/
+
+## Continuous Integration
+
+Build application docker image with node:16-alpine base image and publish it to docker hub.
+https://hub.docker.com/repository/docker/hariprakashohio/my-node-js-app
+
+The below pipeline to run on every code change made to the repository.
+![Build Docker Image](readme-images/build-node-image.PNG)
+
+![E2E Build Deploy Pipeline](readme-images/e2e-pipeline.PNG)
--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,34 @@ let router = express.Router();
 let Books = require('../models/books');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shopping/index', { title: 'Book Store Application' });
+router.get('/', function (req, res, next) {
+    res.render('shopping/index', {title: 'Book Store Application'});
+});
+
+router.get('/health', function (req, res, next) {
+    res.render('shopping/base', {title: "Application is healthy!!"});
+});
+
+router.get('/live', function (req, res, next) {
+    res.render('shopping/base', {title: "Application is live!!"});
 });
 
 
-router.get('/books',function (req, res, next) {
-  let books = Books.find(function (err, docs) {
-    let successMsg = req.flash('success')[0];
-    let bookArray = [];
-    let chunkSize = 3;
-    for (let i = 0; i < docs.length; i += chunkSize) {
-      bookArray.push(docs.slice(i, i + chunkSize))
-    }
-    res.render('shopping/books', { title: 'Buy Books | Welcome', books: bookArray, successMsg: successMsg, noMessages: !successMsg });
-  }).lean();
+router.get('/books', function (req, res, next) {
+    let books = Books.find(function (err, docs) {
+        let successMsg = req.flash('success')[0];
+        let bookArray = [];
+        let chunkSize = 3;
+        for (let i = 0; i < docs.length; i += chunkSize) {
+            bookArray.push(docs.slice(i, i + chunkSize))
+        }
+        res.render('shopping/books', {
+            title: 'Buy Books | Welcome',
+            books: bookArray,
+            successMsg: successMsg,
+            noMessages: !successMsg
+        });
+    }).lean();
 });
 
 module.exports = router;
