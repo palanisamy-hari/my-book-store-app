e57ddbdfeddb83475d7cacaeb9d9299861f024ae
Hariprakash94 <30706291+Hariprakash94@users.noreply.github.com>
2021-11-23 16:37:42 -0500

    feature 2

--- ci/ci-test/features/support/env.rb
+++ ci/ci-test/features/support/env.rb
@@ -5,4 +5,3 @@ require 'active_support'
 require 'rspec'
 
 ENV['FILE_NAME'] = 'test.mp4'
-ENV['SELENIUM-GRID-HOST']="http://3.144.100.236:4444"
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+var Cart = require('../models/cart');
 let Books = require('../models/books');
 
 /* GET home page. */
@@ -32,5 +33,59 @@ router.get('/books', function (req, res, next) {
         });
     }).lean();
 });
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+    var BookId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    Books.findById(BookId, function(err, product){
+        if(err){
+            return res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        res.redirect('/books');
+    });
+});
+
+router.get('/shopping-cart', function(req, res, next) {
+    if(!req.session.cart){
+        return res.render('shopping/shopping-cart', {products: null});
+    }
+    var cart = new Cart(req.session.cart);
+    res.render('shopping/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+
+router.get('/reduce/:id', function(req, res, next) {
+    var BookId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    cart.reduceByOne(BookId);
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function(req, res, next) {
+    var BookId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+    cart.removeItem(BookId);
+    req.session.cart = cart;
+    res.redirect('/shopping-cart');
+});
+
+router.post('/checkout', isLoggedIn, function(req, res, next) {
+    if(!req.session.cart){
+        return res.render('shop/shopping-cart', {products: null});
+    }
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    req.session.oldUrl = req.url;
+    res.redirect('/user/signin');
+}
 
 module.exports = router;
